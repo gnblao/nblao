598a7036c905949845636f0753c0fb54c310dc61
gnblao <gnblao@gmail.com>
2021-05-17 01:15:35 +0800

    update

--- install.sh
+++ install.sh
@@ -7,6 +7,8 @@
 ln -s `pwd`/vimrc ~/.vimrc
 ln -s `pwd`/gitconfig ~/.gitconfig
 ln -s `pwd`/bashrc ~/.bashrc
+ln -s `pwd`/tmux.conf ~/.tmux.conf
+ln -s `pwd`/global.rc ~/.global.rc
 
-cp -r dict ~/.vim/
+#cp -r dict ~/.vim/
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
