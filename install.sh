68bb38c40a2199548f92831a43fcddf2d626d2dc
gnblao <gnblao@gmial.com>
2022-05-19 12:13:24 +0800

    update

--- install.sh
+++ install.sh
@@ -12,25 +12,26 @@ ln -s `pwd`/global.rc ~/.global.rc
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 if [ "/bin/bash" == "$SHELL" ]; then
-    shrc_path="~/.bashrc"
-    $my_bashrc="my_bashrc"
+    shrc_path="$HOME/.bashrc"
+    my_bashrc="my_bashrc"
     
-    flg=`grep "$my_bashrc" $(shrc_path) | wc -l`
+    #flg=`grep "$my_bashrc" ~/.bashrc | wc -l`
+    flg=`grep "$my_bashrc" $shrc_path | wc -l`
     if [ $flg -eq 0 ]; then
-        echo '. '"`pwd`/$my_bashrc" >> $(shrc_path)
+        echo '. '"`pwd`/$my_bashrc" >> $shrc_path
     else
-        sed -i '' "/.*\/$my_bashrc/d" $(shrc_path) &&  echo '. '"`pwd`/$my_bashrc" >> $(shrc_path)
+        sed -i "/.*\/$my_bashrc/d" $shrc_path &&  echo '. '"`pwd`/$my_bashrc" >> $shrc_path
     fi
 fi
 
 if [ "/bin/zsh" == "$SHELL" ]; then
-    shrc_path="~/.zshrc"
-    $my_zshrc="my_zshrc"
+    shrc_path="$HOME/.zshrc"
+    my_zshrc="my_zshrc"
 
-    flg=`grep "$my_zshrc" $(shrc_path) | wc -l`
+    flg=`grep "$my_zshrc" $shrc_path | wc -l`
     if [ $flg -eq 0 ]; then
-        echo '. '"`pwd`/$my_zshrc" >> $(shrc_path)
+        echo '. '"`pwd`/$my_zshrc" >> $shrc_path
     else
-        sed -i '' "/.*\/$my_zshrc/d" $(shrc_path) &&  echo '. '"`pwd`/$my_zshrc" >> $(shrc_path)
+        sed -i '' "/.*\/$my_zshrc/d" $shrc_path &&  echo '. '"`pwd`/$my_zshrc" >> $shrc_path
     fi
 fi
